877eca5ef1939ea8b2c13f9c1f93f3c6687317a2
Thoooomas14 <thomasbianco14@gmail.com>
2026-01-02 17:53:52 -0500

    adding limit to gradient size before running optimizer

--- imitation/train.py
+++ imitation/train.py
@@ -416,6 +416,7 @@ def train(args):
                     loss = action_loss + (requery_loss * args.requery_weight)
                     
                     loss.backward()
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) 
                     optimizer.step()
                     scheduler.step()
                     
