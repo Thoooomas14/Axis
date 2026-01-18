b658249433ac93558098450c1c15a0bc144e35b3
Thoooomas14 <thomasbianco14@gmail.com>
2026-01-18 15:17:58 -0500

    improper tf syntax fix

--- imitation/data/rtx_stream_loader.py
+++ imitation/data/rtx_stream_loader.py
@@ -460,9 +460,7 @@ class RTXStreamLoader(IterableDataset):
         # This prevents TF from accumulating internal buffers
         options = tf.data.Options()
         options.experimental_optimization.apply_default_optimizations = False
-        options.experimental_optimization.autotune = False
-        options.threading.private_threadpool_size = 1
-        options.threading.max_intra_op_parallelism = 1
+        options.autotune.enabled = False
         ds = ds.with_options(options)
             
         # Sharding for PyTorch DataLoader Workers
